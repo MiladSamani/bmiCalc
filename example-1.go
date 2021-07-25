92571258b875ef4b19c9e42373695dab74601e88
“Milad <eng.milad.samani@gmail.com>
2021-07-25 13:49:47 +0430

    variables

---
+++ example-1.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main()  {
+	//=========one declare=======
+	//firstName := "Milad"
+	//lastName := "Samani"
+	//fmt.Printf("FirstName : %s and LastName : %s" , firstName , lastName)
+	//=========two declare=======
+	//var firstName string = "Milad"
+	//var lastName string = "Samani"
+	//fmt.Printf("FirstName : %s and LastName : %s" , firstName , lastName)
+	currentYear:= 2021
+	myBirthday := 1998
+	Dispute := currentYear - myBirthday
+ 	fmt.Printf("the current Year  : %d and your Birthday :  %d and Dispute : %d" , currentYear , myBirthday , Dispute)
+}
